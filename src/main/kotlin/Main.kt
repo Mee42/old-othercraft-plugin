a79e28ae322f23ba2e35a5b0b8680fb14d37eb85
carson <carson42g@gmail.com>
2019-07-16 20:16:24 -0400

    h

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -175,6 +175,7 @@ class OthercraftPlugin : JavaPlugin() {
             .switchIfEmpty {
                 discord.statusChannel.flatMap { channel -> channel.createEmbed { u -> spec(u) } }
             }
+            .onErrorMap { it.printStackTrace() }
             .subscribe()
     }
 
